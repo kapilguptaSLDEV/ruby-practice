1b6c3f03841d8116a4ccaf2593467406d38a56d3
kapilguptaSLDEV <kapil.gupta@splashlearn.com>
2021-10-03 13:40:29 +0530

    added to json

--- common.rb
+++ common.rb
@@ -6,8 +6,12 @@ load 'suv.rb'
 load 'sedan.rb'
 load 'hatchback.rb'
 
+require 'json'
+
 v = Vehicle.new('Suzuki', '2019', 'Baleno', 4)
 v1 = Truck.new('Suzuki', '2020', 'Truck', 8)
 v2 = Bus.new('Suzuki', '2018', 'Green', 6)
 v3 = Vehicle.new('Suzuki', '2012', 'WagonR', 4)
-puts Vehicle.get_hash;
+v4 = Car.new('Jeep', '2018', 'Compass', 4)
+
+puts Vehicle.get_hash.to_json;
